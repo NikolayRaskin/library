733545b2ad09e14b019113f0f151475b903db7c3
Nikolay Raskin <n.woots98@gmail.com>
2019-09-08 15:12:09 +0300

    rest_api

---
+++ library/base/serializers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from base.models import User_Profile, Book
+from rest_framework import serializers
+
+
+class User_ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User_Profile
+        fields = '__all__'
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
--- library/user_profile/urls.py
+++ library/user_profile/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('/<int:id>',views.user_profile,name = 'user_profile'),
+    path('<int:id>',views.user_profile,name = 'user_profile'),
     path('removeBook/<int:id>',views.removeBook,name = 'removeBook'),
     path('editBook/<int:id>',views.editBook,name = 'editBook'),
 ]
